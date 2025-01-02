a1786d65ef95428b9e34a51e88ab02fe2453aa98
Raymond023 <raymondraphael11@gmail.com>
2025-01-02 03:52:51 +0100

    cart, minus and plus buttons click features added

--- script.js
+++ script.js
@@ -14,3 +14,42 @@ closeNav.addEventListener('click', () => {
     navBar.classList.remove('show-nav');
     overlay.classList.remove('show-overlay');
 })
+
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const quantity = document.querySelector('.quantity')
+
+let counter = 0;
+
+function totalQuantity() {
+    plus.addEventListener('click', () => {
+        counter++
+        quantity.textContent = counter;
+    })
+    minus.addEventListener('click', () => {
+        if(counter > 0){
+            counter--
+            quantity.textContent = counter;
+        }
+
+    })
+
+}
+totalQuantity()
+
+const cart = document.querySelector('.cart');
+const cartContent = document.querySelector('.cart-content');
+const checkout = document.querySelector('.checkout');
+
+cart.addEventListener('click', () => {
+    
+    if(cartContent.style.display === 'block') {
+        cartContent.style.display = 'none'
+    } else {
+        cartContent.style.display = 'block'
+    }
+})
+
+// checkout.addEventListener('click', () => {
+//     cartContent.style.display = 'none'
+// })
