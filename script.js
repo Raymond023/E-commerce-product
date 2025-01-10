96ed1e30825e7385a8e45cb0aad929d794e9f9f2
Raymond023 <raymondraphael11@gmail.com>
2025-01-10 09:24:15 +0100

    added gallery feature for image

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 'use strict';
 
+// nav function
+const body = document.body;
 const closeNav = document.querySelector('.close-nav');
 const openNav = document.querySelector('.open-nav');
-const navBar = document.querySelector('nav');
-const overlay = document.querySelector('.overlay');
+const navDialog = document.querySelector('.nav-dialog')
 
 openNav.addEventListener('click', () => {
-    navBar.classList.add('show-nav');
-    overlay.classList.add('show-overlay');
+    body.classList.toggle('overlay');
+    navDialog.classList.add('show-nav');
+    navDialog.showModal();
 });
 
 closeNav.addEventListener('click', () => {
-    navBar.classList.remove('show-nav');
-    overlay.classList.remove('show-overlay');
+    body.classList.toggle('overlay');
+    navDialog.classList.remove('show-nav');
+    navDialog.close();
 })
+
+navDialog.addEventListener('click', (e) =>{
+    if(e.target === navDialog) {
+        navDialog.close();
+        body.classList.toggle('overlay');
+    }
+})
+
+// end of nav function
+
 
 const plus = document.querySelector('.plus');
 const minus = document.querySelector('.minus');
-const quantity = document.querySelector('.quantity')
+const quantity = document.querySelector('.quantity');
+
 
 let counter = 0;
 
@@ -31,6 +45,8 @@ function totalQuantity() {
             counter--
             quantity.textContent = counter;
         }
+        quantityBought.textContent = discountPrice.textContent + ' x ' + quantity.textContent;
+        tototalCost()
 
     })
 
@@ -44,12 +60,64 @@ const checkout = document.querySelector('.checkout');
 cart.addEventListener('click', () => {
     
     if(cartContent.style.display === 'block') {
-        cartContent.style.display = 'none'
+        cartContent.classList.toggle('hide');
     } else {
-        cartContent.style.display = 'block'
+        cartContent.classList.toggle('show');
     }
 })
 
-// checkout.addEventListener('click', () => {
-//     cartContent.style.display = 'none'
-// })
+const discountPrice = document.querySelector('.discount-price');
+const discountPriceValue = discountPrice.textContent.replace('$', '');
+const result = document.querySelector('.result');
+const additem = document.querySelector('.add-item');
+
+
+
+function tototalCost () {
+    additem.addEventListener('click', () => {
+        if(!quantity.textContent == 0|| !quantity.textContent == ''){
+         
+        let resuitText = parseFloat(discountPriceValue) * parseFloat(quantity.textContent);
+        result.textContent = '$' + resuitText.toFixed(2)
+        }
+    })
+
+}
+
+tototalCost()
+
+
+const nameOfProduct = document.querySelector('#name');
+if(result.textContent != '0') {
+    nameOfProduct.textContent = document.querySelector('h2').textContent;
+}
+
+const quantityBought = document.querySelector('#purchase-quantity');
+
+
+// gallery
+const displayedImage = document.querySelector('#displayedImage');
+const previous = document.querySelector('.previous');
+const next = document.querySelector('.next');
+
+let currentInageIndex = 0;
+let images = [
+    'images/image-product-1.jpg',
+    'images/image-product-2.jpg',
+    'images/image-product-3.jpg',
+    'images/image-product-4.jpg'    
+];
+
+function changeImage (direction) {
+    currentInageIndex += direction;
+
+    if(currentInageIndex >= images.length) {
+        currentInageIndex = 0;
+    } else if (currentInageIndex < 0) {
+        currentInageIndex = images.length -1;
+    }
+    displayedImage.src =images[currentInageIndex];
+}
+
+next.addEventListener('click', function() {changeImage(+1)});
+previous.addEventListener('click', function() {changeImage(-1)});
